9273b06ecde9bda2e5e61586133238044a0c1d52
Bob Krijnen <krijnenbob@live.nl>
2018-12-17 17:14:55 +0100

    Update filter.js

--- resources/js/filter.js
+++ resources/js/filter.js
@@ -1,5 +1,5 @@
 import FilterSelector from './components/FilterSelector'
 
 Nova.booting(Vue => {
-    Vue.component('filter-selector', Vue.component('filter-selector').extend(FilterSelector));
+    Vue.component('filter-selector', FilterSelector).extend(FilterSelector);
 });
